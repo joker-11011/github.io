5f775630af9857faf050bd680bd68bf7d8eae6ed
joker-11011 <eeshanmanja2@gmail.com>
2024-07-19 01:10:29 +0530

    updated the design

--- script.js
+++ script.js
@@ -4,11 +4,54 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const commands = {
         help: 'Available commands: help, about, skills, education, experience, contact, clear',
-        about: 'Eeshan Pradeep Manja: Programming Enthusiast, highly motivated and eager to start a career.',
-        skills: 'Skills: C/C++, Python, AWS Cloud, SQL, Snowflake, HTML, CSS, Django, JavaScript, Problem Solving',
-        education: `Education:\n- The National Institute of Engineering, Mysore: BE, Electronics and Communication, CGPA: 7.3\n- Sharada PU College, Mangaluru: Pre-University, 89.16%\n- Sri Satya Sai Loka Seva Vidya Kendra, Alike, D.K: High School, 79%`,
-        experience: `Internship:\n- National Institute of Technology, Suratkal: NITK - IEEE Photonics Student Internship Program, June 2022\n  Focused on designing a ring resonator for cancer cell detection using optiFDTD software.\n\nAdditional Experience:\n- XYZ Corporation: Software Development Intern, January 2023 - June 2023\n  Worked on developing and optimizing machine learning models for predictive analytics. Implemented various data processing pipelines and contributed to backend development using Python and Django.`,
-        contact: 'Contact Info:\nPhone: <a href="tel:+919483975250">+91 9483975250</a>\nEmail: <a href="mailto:eeshanmanja2@gmail.com">eeshanmanja2@gmail.com</a>\nLocation: Sagar, Karnataka, India',
+        about: 'Eeshan Pradeep Manja: Data Scientist, highly motivated and eager to start a career.',
+        skills: 'C, C++, Python, JavaScript, AWS Cloud, SQL, HTML, CSS, Shell Scripting',
+        education: `
+<h>* commit Bachelor Degree</h>
+| School: The National Institute of Engineering, Mysore
+| Date:   2024
+|
+|     - Electronics and Communication
+|     - CGPA: 7.41
+|
+<h>* commit Pre-University</h>
+| School: Sharada PU College, Mangaluru
+| Date:   2020
+|
+|     - 89.16%
+|
+<h>* commit High School</h>
+| School: Sri Satya Sai Loka Seva Vidya Kendra, Alike, D.K
+| Date:   2018
+|
+|     - 79%
+        `,
+        experience: `
+<h>* Role: Data Scientist</h>
+| Company: Knowledge Foundry Business Solutions
+| Date:   July 2024 
+|        
+<h>* Role: Intern</h>
+| Company: Knowledge Foundry Business Solutions
+| Date:   March 2024
+|
+|     - Developed a computer vision solutions using OpenCV, enhancing image processing and analysis capabilities.
+|     - Utilized YOLO (You Only Look Once) for real-time object detection projects, improving detection accuracy and performance.
+|     - Worked on different AWS cloud services.
+|     - Gained hands-on experience in various stages of project development, from conceptualization to deployment.
+|
+<h>* Role: IEEE Photonics Student Intern</h>
+  Company: National Institute of Technology, Suratkal
+  Date:   June 2022
+ 
+      - Focused on designing a ring resonator for cancer cell detection using optiFDTD software.
+        `,
+        certificates: `
+<a href="https://coursera.org/verify/DCZNHM8GD4G4">Supervised Machine Learning: Regression and Classification</a>
+<a href="https://www.credly.com/badges/2f332fda-1a77-4e9f-8701-ada189e1db0c/linked_in_profile">AWS Knowledge: Cloud Essentials</a>
+        `,
+
+        contact: 'Phone: <a href="tel:+919483975250">+91 9483975250</a> Email: <a href="mailto:eeshanmanja2@gmail.com">eeshanmanja2@gmail.com</a>',
         clear: ''
     };
 
